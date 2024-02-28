aa79d16bc183e2b7c71feb1a0f993a73fb404e87
Zach <zachalba@uri.edu>
2024-02-28 13:06:00 -0500

    no jquery

--- js/book_html.js
+++ js/book_html.js
@@ -1,23 +1,40 @@
-function opacity() {
-    $('img').css('opacity', '0.5');
-}
-function trigger(path){
-    function makeRequest() {
-        var xhr = new XMLHttpRequest();
-        xhr.onload = function() {
-            if(xhr.status === 200) {
-                $('#details').html(xhr.responseText);
-            }   
-        };
-
+function changeImageOpacity(){
+    var images = document.getElementsByTagName('img');
+    for (var i =0; i<images.length; i++){
+        images[i].style.opacity = 0.5;
     }
-    makeRequest();
-    xhr.open('GET', path, true);
-    xhr.send(null);
 }
 
-$('#don-quixote-img').on('click', function() {
-    trigger('data/cervantes_data.html');
-    opacity();
-    this.css('opacity', '1.0');
+function loadHTMLFile(filePath){
+    var xhr = new XMLHttpRequest();
+    xhr.onload = function(){
+        if(xhr.status === 200){
+            document.getElementById('details').innerHTML = xhr.responseText;
+
+        }
+    }
+    xhr.open('GET', filePath, true);
+    xhr.send();
+}
+
+var donQuixoteImage = document.getElementById('don-quixote-img');
+donQuixoteImage.addEventListener('click', function(){
+    changeImageOpacity();
+    loadHTMLFile('data/cervantes_data.html');
+    donQuixoteImage.style.opacity = 1;
 });
+
+var taleOfTwoCitiesImage = document.getElementById('two-cities-img');
+taleOfTwoCitiesImage.addEventListener('click', function(){
+    changeImageOpacity();
+    loadHTMLFile('data/dickens_data.html');
+    taleOfTwoCitiesImage.style.opacity = 1;
+});
+
+var lordOfTheRingsImage = document.getElementById('lotr-img');
+lordOfTheRingsImage.addEventListener('click', function(){
+    changeImageOpacity();
+    loadHTMLFile('data/tolkien_data.html');
+    lordOfTheRingsImage.style.opacity = 1;
+});
+
